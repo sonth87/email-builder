a2a736bed8ed0932f6ebbcf4584d6928e56bf746
Skyline <>
2025-05-28 13:52:37 +0700

    change app's name

--- packages/block-columns-container/src/index.tsx
+++ packages/block-columns-container/src/index.tsx
@@ -39,7 +39,19 @@ export const ColumnsContainerPropsSchema = z.object({
     .object({
       fixedWidths: FIXED_WIDTHS_SCHEMA,
       columnsCount: z
-        .union([z.literal(2), z.literal(3)])
+        .union([
+          z.literal(2),
+          z.literal(3),
+          z.literal(4),
+          z.literal(5),
+          z.literal(6),
+          z.literal(7),
+          z.literal(8),
+          z.literal(9),
+          z.literal(10),
+          z.literal(11),
+          z.literal(12),
+        ])
         .optional()
         .nullable(),
       columnsGap: z.number().optional().nullable(),
@@ -98,6 +110,16 @@ export function ColumnsContainer({
             <TableCell index={0} props={blockProps} columns={columns} />
             <TableCell index={1} props={blockProps} columns={columns} />
             <TableCell index={2} props={blockProps} columns={columns} />
+            <TableCell index={3} props={blockProps} columns={columns} />
+            <TableCell index={4} props={blockProps} columns={columns} />
+            <TableCell index={5} props={blockProps} columns={columns} />
+            <TableCell index={6} props={blockProps} columns={columns} />
+            <TableCell index={7} props={blockProps} columns={columns} />
+            <TableCell index={8} props={blockProps} columns={columns} />
+            <TableCell index={9} props={blockProps} columns={columns} />
+            <TableCell index={10} props={blockProps} columns={columns} />
+            <TableCell index={11} props={blockProps} columns={columns} />
+            <TableCell index={12} props={blockProps} columns={columns} />
           </tr>
         </tbody>
       </table>
@@ -108,7 +130,7 @@ export function ColumnsContainer({
 type Props = {
   props: {
     fixedWidths: z.infer<typeof FIXED_WIDTHS_SCHEMA>;
-    columnsCount: 2 | 3;
+    columnsCount: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
     columnsGap: number;
     contentAlignment: "top" | "middle" | "bottom";
   };
@@ -138,7 +160,7 @@ function TableCell({ index, props, columns }: Props) {
 
 function getPaddingBefore(
   index: number,
-  { columnsGap, columnsCount }: Props["props"],
+  { columnsGap, columnsCount }: Props["props"]
 ) {
   if (index === 0) {
     return 0;
@@ -154,7 +176,7 @@ function getPaddingBefore(
 
 function getPaddingAfter(
   index: number,
-  { columnsGap, columnsCount }: Props["props"],
+  { columnsGap, columnsCount }: Props["props"]
 ) {
   if (columnsCount === 2) {
     if (index === 0) {
